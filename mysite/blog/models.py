c2fd0eb86618cb2a301a93cc4a2b5dc49c2cbb8e
Ilya <zhilov1945@gmail.com>
2022-06-04 09:42:29 +0300

    fixed bug and registing Comment in admin

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -55,6 +55,5 @@ class Comment(models.Model):
             models.Index(fields=['created']),
         ]
 
-
-def __str__(self):
-    return f'Comment by {self.name} on {self.post}'
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
