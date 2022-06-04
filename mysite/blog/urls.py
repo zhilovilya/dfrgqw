45b461671686d5dd31e29f2efdb1397be3450392
Ilya <zhilov1945@gmail.com>
2022-06-04 10:09:50 +0300

    can see comments at the blog

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment')
 ]
