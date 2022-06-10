6cf44ba17addc9d4c7c0ba4834df0f7da5bf47d9
NealLegweak <Dvornikovvdanil@yandex.ru>
2022-06-10 10:42:51 +0300

    all my work

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog'))
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('', include('blog.urls', namespace='blog'))
 ]
